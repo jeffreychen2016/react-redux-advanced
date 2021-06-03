6fb895c4810629afd636d9d54564abd35dddcbc4
JeffreyChen <jeffreychen@JeffreyChens-MacBook-Pro.local>
2021-06-02 19:54:59 -0500

    feat: add logic for sending data to firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,106 @@
+import React from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+import * as appSetting from "./appSetting";
+
+// to prevent sending empty data to firebase when component first mount (page refreshed)
+let isInitial = true;
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
 
+  // again, when use `useSelector`, it automatically subscribe the component to the data store
+  // which means, every time the `cart` states change, the App component and its children
+  // get re-evaluated
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+
+  // *** IMPORTANT ***
+  // we are going to send request
+  // whenever the cart changes
+  // that means when we click on the + button
+  // it will add it to the cart
+  // then this component gets re-evaluated since the `cart` state changed
+  // then this useEffect add data to the firebase
+  useEffect(() => {
+    const sendCartData = async () => {
+      // pending
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+
+      const response = await fetch(appSetting.firebaseUrlCart, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        // error
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data failed!",
+          })
+        );
+
+        throw new Error("Sending cart data failed.");
+      }
+
+      // success
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      // error
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: error.message,
+        })
+      );
+    });
+    // like other react hooks
+    // react-redux will garuntee thst dispatch function never change
+    // but, still add it here
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <React.Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </React.Fragment>
   );
 }
 
---
+++ src/appSetting.js
@@ -0,0 +1,4 @@
+const firebaseUrlCart =
+  "https://react-food-app-4b58b-default-rtdb.firebaseio.com/cart.json";
+
+export { firebaseUrlCart };
